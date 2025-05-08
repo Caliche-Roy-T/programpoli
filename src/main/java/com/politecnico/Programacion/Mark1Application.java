000250178c124986931e56a5dd767774883c9571
Roy-T <RoyT5@gmail.com>
2025-05-08 09:45:53 -0500

    first commit

---
+++ src/main/java/com/politecnico/Programacion/Mark1Application.java
@@ -0,0 +1,13 @@
+package com.politecnico.Programacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mark1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mark1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/politecnico/Programacion/Mark1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.politecnico.Programacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mark1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
