ca54bf99bce544c1b7e7ae9e32bfaab333b4f91f
Roy-T <RoyT5@gmail.com>
2025-05-20 10:06:31 -0500

    ccgz

---
+++ src/main/java/com/politecnico/Programacion/models/Usuario.java
@@ -0,0 +1,14 @@
+package com.politecnico.Programacion.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+
+public class Usuario {
+    
+
+}
